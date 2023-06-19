4356caeaba5cc865bceb71738eb7f95766218fcb
J-D95 <juand.canton@gmail.com>
2023-06-18 21:05:42 -0700

    eigth attempt

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,14 +9,14 @@ class Square:
 
         Args:
             size (int): The size of the new square.
-            """
+         """
 
-            if not isinstance(size, int):
-                raise TypeError("size must be an interger")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an interger")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-            def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
